897827aec15425e5afa414503225559c54f116c9
Yash Singhal <y.yashsinghal207@gmail.com>
2024-08-07 18:50:00 +0530

    DAY 2.cpp

--- DAY 2.cpp
+++ DAY 2.cpp
@@ -1,18 +1,18 @@
-// #Assignment 1
+ #Assignment 1
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     char ab;
-//     cout<<"Enter the Character: ";
-//     cin>>ab;
-//     if(ab=='a'||ab=='e'||ab=='i'||ab=='o'||ab=='u'||ab=='A'||ab=='E'||ab=='I'||ab=='O'||ab=='U'){
-//         cout<<"Vowel";
-//     }
-//     else{
-//         cout<<"Consonant";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main(){
+     char ab;
+     cout<<"Enter the Character: ";
+     cin>>ab;
+     if(ab=='a'||ab=='e'||ab=='i'||ab=='o'||ab=='u'||ab=='A'||ab=='E'||ab=='I'||ab=='O'||ab=='U'){
+         cout<<"Vowel";
+     }
+     else{
+         cout<<"Consonant";
+     }
+}
 
 
 //Assignment 2
@@ -36,4 +36,41 @@ int main(){
     else{
         cout<<"%d is Greater."<<num3; 
     }
+}
+
+
+//Assignment 3
+
+#include<iostream>
+using namespace std;
+void func(int a,int b,int c){
+    int res = a+b+c;
+    cout<<"Sum is "<<res;
+}
+int main(){
+    int a,b,c;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    cout<<"Enter Second Numeber: ";
+    cin>>b;
+    cout<<"Enter Third Number: ";
+    cin>>c;
+    func(a,b,c);
+}
+
+
+//Assignment 4
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+void func(int a){
+    int res = sqrt(a);
+    cout<<res<<" is the Squareroot.";
+}
+int main(){
+    int a;
+    cout<<"Enter Number: ";
+    cin>>a;
+    func(a);
 }
